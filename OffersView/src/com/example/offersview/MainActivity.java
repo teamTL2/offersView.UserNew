27efad9662ae6b35ec470792c4082e399063b83c
Stavros <msensis@msensis-Latitude-E5510>
2015-01-09 17:35:07 +0200

    MapView 1.0.1

--- OffersView/src/com/example/offersview/MainActivity.java
+++ OffersView/src/com/example/offersview/MainActivity.java
@@ -5,8 +5,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.example.offersview.activities.MapViewActivity;
 import com.example.offersview.activities.NearestShopsActivity;
 import com.example.offersview.activities.ShopListActivity;
  
@@ -14,6 +16,7 @@ public class MainActivity extends Activity{
  
     Button btnViewShops;
     Button btnViewNearestShops;
+    Button btnMapViewNearestShops;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,7 @@ public class MainActivity extends Activity{
         // Buttons
         btnViewShops = (Button) findViewById(R.id.btnViewShops);
         btnViewNearestShops = (Button) findViewById(R.id.btnViewNearestShops);
-        
+        btnMapViewNearestShops = (Button) findViewById(R.id.btnMapViewNearestShops);
         
         // view all shops click event
         btnViewShops.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +39,15 @@ public class MainActivity extends Activity{
  
             }
         });
+        
+        btnMapViewNearestShops.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				 Intent i = new Intent(getApplicationContext(), MapViewActivity.class);
+	                startActivity(i);
+			}
+		});
         
         
         btnViewNearestShops.setOnClickListener(new View.OnClickListener() {
